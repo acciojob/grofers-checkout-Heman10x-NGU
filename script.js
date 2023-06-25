04e75414d120bb5e2456b1b357436e162a9507a0
Heman10x-NGU <80228113+Heman10x-NGU@users.noreply.github.com>
2023-06-25 15:43:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ const getSum = () => {
   // Step 2: Calculate the total price
   let totalPrice = 0;
   prices.forEach((price) => {
-    totalPrice += parseFloat(price.textContent);
+    totalPrice += parseInt(price.textContent);
   });
   // Step 3: Create a new row for the total price
   const table = document.querySelector('table');
   const newRow = document.createElement('tr');
   const newCell = document.createElement('td');
-  newCell.textContent = 'Total Price: ' + totalPrice.toFixed(2); // Assuming prices are in decimal format
-  newCell.setAttribute('colspan', '2'); // Adjust the colspan according to the number of columns in your table
+  newCell.setAttribute('id', 'ans');
+  newCell.textContent = totalPrice
   newRow.appendChild(newCell);
   table.appendChild(newRow);
   
